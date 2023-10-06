fa1e08e70d52e7af941fec5e71b635ef1def3128
heko <91366057+hkohko@users.noreply.github.com>
2023-10-06 21:53:26 +0700

    created queries and stored song in db

---
+++ scrape_osu_songs/db/sql_query.py
@@ -0,0 +1,44 @@
+import sqlite3
+from pathlib import PurePath
+
+DB_DIR = PurePath(__file__).parents[0]
+DB_FILE = DB_DIR.joinpath("database_file", "osu_songs.db")
+
+
+def connect() -> sqlite3.Connection:
+    return sqlite3.connect(DB_FILE)
+
+
+def create_tables():
+    Q_CREATE_TABLES = """CREATE TABLE IF NOT EXISTS songs(
+    id INTEGER PRIMARY KEY,
+    artist TEXT,
+    title TEXT,
+    preview TEXT
+    )STRICT
+    """
+
+    with connect() as conn:
+        cursor = conn.cursor()
+        cursor.execute(Q_CREATE_TABLES)
+
+
+def insert_into_db(data: list[dict]):
+    Q_INSERT_INTO_SONGS = """INSERT OR IGNORE INTO songs(
+    artist,
+    title,
+    preview
+    ) VALUES(
+    :artist,
+    :title,
+    :preview
+    )
+    """
+    with connect() as conn:
+        cursor = conn.cursor()
+        cursor.executemany(Q_INSERT_INTO_SONGS, data)
+        conn.commit()
+
+
+if __name__ == "__main__":
+    pass
