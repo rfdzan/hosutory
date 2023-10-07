a915a67eed3254316101bf154d4042e87918b271
heko <91366057+hkohko@users.noreply.github.com>
2023-10-07 16:13:58 +0700

    reflect 'source' in scrape and search

--- scrape_osu_songs/db/search_db.py
+++ scrape_osu_songs/db/search_db.py
@@ -8,7 +8,7 @@ def raise_error() -> None:
 
 
 def sanitize_input():
-    choice = ("artist", "title", "preview")
+    choice = ("artist", "title", "preview", "source")
 
     by = input("where: ")
     if by not in choice:
@@ -22,13 +22,11 @@ def sanitize_input():
 def query_the_db():
     select, by, like = sanitize_input()
     header = select
-    q_select = (
-        f"SELECT artist, title, preview FROM songs WHERE {by} LIKE :like ORDER BY title"
-    )
+    q_select = f"SELECT artist, title, preview, source FROM songs WHERE {by} LIKE :like ORDER BY title"
 
     with connect() as conn:
         cursor = conn.cursor()
         cursor.execute(q_select, {"like": f"%{like}%"})
-        table = [(data[0], data[1], f"https:{data[2]}") for data in cursor]
+        table = [(data[0], data[1], f"https:{data[2]}", data[3]) for data in cursor]
 
     print(tabulate(table, headers=header, tablefmt="grid"))
