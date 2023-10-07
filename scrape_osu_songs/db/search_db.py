caf91bff69cc072b2ff1e9ed9ca9beab8064c639
heko <91366057+hkohko@users.noreply.github.com>
2023-10-07 08:35:20 +0700

    tabulate, searches all columns in db instead

--- scrape_osu_songs/db/search_db.py
+++ scrape_osu_songs/db/search_db.py
@@ -1,4 +1,5 @@
-from sql_query import connect
+from .sql_query import connect
+from tabulate import tabulate
 
 
 def raise_error() -> None:
@@ -6,33 +7,27 @@ def raise_error() -> None:
 
 
 def sanitize_input():
-    choice = ("artist", "title", "preview", ",")
-    select = input("select: ")
-    if "," in select:
-        select = select.split(",")
-        for word in select:
-            if word.strip() not in choice:
-                raise_error()
-    else:
-        if select.strip() not in choice:
-            raise_error()
+    choice = ("artist", "title", "preview")
 
     by = input("where: ")
     if by not in choice:
         raise_error()
+
     like = input("like: ")
 
-    return select, by, like
+    return choice, by, like
 
 
 def query_the_db():
     select, by, like = sanitize_input()
-    if isinstance(select, list):
-        select_strip = [word.strip() for word in select]
-        select = ", ".join(select_strip)
-    q_select = f"SELECT {select} FROM songs WHERE {by} LIKE :like"
+    header = select
+    q_select = (
+        f"SELECT artist, title, preview FROM songs WHERE {by} LIKE :like ORDER BY title"
+    )
+
     with connect() as conn:
         cursor = conn.cursor()
         cursor.execute(q_select, {"like": f"%{like}%"})
-        for data in cursor:
-            print(data)
+        table = [(data[0], data[1], f"https:{data[2]}") for data in cursor]
+
+    print(tabulate(table, headers=header, tablefmt="grid"))
