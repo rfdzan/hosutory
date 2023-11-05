580864398d7b01fe1c2babbaa879e088967f4496
heko <91366057+hkohko@users.noreply.github.com>
2023-11-05 20:01:36 +0700

    added bash scripts linux platform option

--- run_scrape.py
+++ run_scrape.py
@@ -1,6 +1,9 @@
 from pathlib import PurePath
 from subprocess import run
+from sys import platform
 
 start_gui = PurePath(__file__).parents[0].joinpath("scrape_osu_songs", "scrape_gui.py")
 python = PurePath(__file__).parents[0].joinpath(".venv", "Scripts", "python.exe")
+if platform == "linux":
+    python = PurePath(__file__).parents[0].joinpath(".venv", "bin", "python")
 run([python, start_gui])
